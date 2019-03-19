e7d1cacb86d2051fe00e720995c33da32b84ee90
John Robertson <tiktaalikdesign@gmail.com>
2019-03-19 19:37:48 -0400

    values storing on click, random number generator working, update user guess working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,21 @@
-console.log('Run the Jewels!')
+console.log('Run the Jewels!'); // just for fun - looks js file and jquery loaded
+let input = '';
+let randomNumber = '';
+let userNumber = 0;
+
+$(document).ready(function () {
+    // Populating our random number
+    randomNumber = Math.floor(Math.random()*50);
+    console.log('The random number is ' + randomNumber);
+
+    // BEGIN USER INPUT - getting out user input here - clicking gem buttons stores the associated html attribute value
+    $('.gem').click(function () {
+        input = parseInt(($(this).val())); // stores value and converts to an integer
+        console.log('This gem is worth ' + input + ' points.');
+        // updateUserNumber();
+        userNumber = userNumber + input;
+    });
+   
+
+});
+
